2f3d412f75521340f41b07c7ce8007bdd295b127
222020350 <222020350@stud.cut.ac.za>
2026-05-20 19:58:46 +0200

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,17 +1,3 @@
 // GROUP MEMBERS: [TH MOSIA 222040802, MC MOGOTSI 221023182, TL MOLOI 222004939, SM NKOSI 222020350, K LETELE 223053487]
 
-class Constants {
-  // From Supabase: Project Settings → API Keys → Project URL
-  static const String supabaseUrl = 'https://vserhpvhohmgsohoqgyj.supabase.co';
-  
-  // From Supabase: Project Settings → API Keys → anon public
-  static const String supabaseAnonKey = 'sb_publishable_xWkcvy1XpJmPlDo7DQCJ5A_fVtyPdoS';
-  
-  // Table names (must match your database)
-  static const String usersTable = 'users';
-  static const String modulesTable = 'modules';
-  static const String applicationsTable = 'applications';
-  
-  // Storage bucket
-  static const String documentsBucket = 'application_docs';
-}
+
