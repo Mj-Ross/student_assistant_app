f2e61b274e1380e8dc87ec32d11fbbecb39896ad
222020350 <222020350@stud.cut.ac.za>
2026-05-20 18:47:26 +0200

    Entered group members in home_viewmodel.dart

--- lib/home/home_viewmodel.dart
+++ lib/home/home_viewmodel.dart
@@ -1,5 +1,4 @@
-// GROUP MEMBERS: [Full Names and Student Numbers]
-
+// GROUP MEMBERS: [TH MOSIA 222040802, MC MOGOTSI 221023182, TL MOLOI 222004939, SM NKOSI 222020350, K LETELE 223053487]
 import 'package:flutter/material.dart';
 import '../providers/auth_provider.dart';
 
