7ed8934f615b7b01fa83df5e5a889bf22e9cbd36
222020350 <222020350@stud.cut.ac.za>
2026-05-20 19:09:55 +0200

    Update application_detail_screen.dart

--- lib/views/student/application_detail_screen.dart
+++ lib/views/student/application_detail_screen.dart
@@ -1,3 +1,4 @@
+// GROUP MEMBERS: [TH MOSIA 222040802, MC MOGOTSI 221023182, TL MOLOI 222004939, SM NKOSI 222020350, K LETELE 223053487]
 // lib/views/student/application_detail_screen.dart
 
 import 'package:flutter/material.dart';
